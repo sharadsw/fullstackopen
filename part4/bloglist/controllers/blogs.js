dc2e603226d32996cb9e53343dfa7af345f77ac4
TheRealSS <sharad97@gmail.com>
2020-11-18 22:24:25 +0530

    upto 4.12

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,22 +1,28 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  
+  response.json(blogs)
 })
   
-blogsRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
-  
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
+blogsRouter.post('/', async (request, response) => {  
+  if (!request.body.title || !request.body.author) {
+    response.status(400).json({
+      error: 'title or author missing'
     })
+  }
+
+  const blog = {
+    title: request.body.title,
+    author: request.body.author,
+    url: request.body.url,
+    likes: request.body.likes || 0
+  }
+
+  const result = await new Blog(blog).save()
+  response.status(201).json(result)
 })
 
 module.exports = blogsRouter
