25328c90809c341cceebdbf901f0118b0edc1e75
TheRealSS <sharad97@gmail.com>
2020-12-09 19:32:23 +0530

    upto 4.21

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -73,7 +73,28 @@ blogsRouter.put('/:id', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndDelete(request.params.id)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!request.token || !decodedToken) {
+    return response.status(401).json({
+      error: 'token missing or invalid'
+    })
+  }
+
+  const blog = await Blog.findById(request.params.id)
+
+  if (!blog) {
+    return response.status(400).json({
+      error: 'invalid blog'
+    })
+  }
+
+  if (decodedToken.id !== blog.user.toString()) {
+    return response.status(401).json({
+      error: 'not authorized to delete this blog'
+    })
+  }
+
+  await Blog.findByIdAndDelete(blog._id)
   response.status(204).end()
 })
 
